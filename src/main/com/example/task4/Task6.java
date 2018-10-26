6773407c1bfc015d7bffaf70f42e35b796baa84b
KKaretina <kcx_1@mail.ru>
2018-10-26 17:11:57 +0300

    тема 5 задача 6

--- src/main/com/example/task4/Task6.java
+++ src/main/com/example/task4/Task6.java
@@ -5,12 +5,22 @@ import java.util.Scanner;
 public class Task6 {
     public static void main(String[] args) {
 
-        int x = 0;
-        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите x,y или z");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
 
-        if (scanner.hasNextInt()) {
-            x = scanner.nextInt();
+        switch (input.charAt(0)) {
+            case 'x':
+                System.out.println("Вы ввели x");
+                break;
+            case 'y':
+                System.out.println("Вы ввели y");
+                break;
+            case 'z':
+                System.out.println("Вы ввели z");
+                break;
+            default:
+                System.out.println("Вы ввели неправильное число");
         }
-        System.out.println(x);
     }
 }
