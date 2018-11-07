d6710da581b11d7dd79436656c958e5ddc8464a5
KKaretina <kcx_1@mail.ru>
2018-11-07 15:30:41 +0300

    task 10_16

--- src/main/com/example/task4/Task10_16.java
+++ src/main/com/example/task4/Task10_16.java
@@ -4,11 +4,16 @@ import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Task10_16 {
+
+    private static final String STOP_WORD = "stop";
+
     public static void main(String[] args) {
-        try (FileWriter writer = new FileWriter("notes5.txt")) {
+        try (FileWriter writer = new FileWriter(Task10_16.class.getSimpleName() + ".txt")) {
             Scanner scanner = new Scanner(System.in);
-            String input = scanner.next();
-            writer.write(input);
+            String input;
+            while (!STOP_WORD.equals(input = scanner.next())) {
+                writer.write(input + "\n");
+            }
         } catch (java.io.IOException ex) {
             System.out.println(ex.getMessage());
         }
